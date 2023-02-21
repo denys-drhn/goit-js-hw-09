e1556659a0b6f95ff3a15125af2f35fc573ea48f
Denys <denys.drhn@gmail.com>
2023-02-22 01:19:38 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyElement = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+const onStartBtnClick = () => {
+	startChangeColor = setInterval(() => {
+		bodyElement.style.backgroundColor = getRandomHexColor();
+	}, 1000);
+	startBtn.disabled = true;
+};
+
+const onStopBtnClick = () => {
+	clearInterval(startChangeColor);
+	startBtn.disabled = false;
+};
+
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+
 
